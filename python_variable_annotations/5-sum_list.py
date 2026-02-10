9b0a21b79bb32646c2795901cbb2b4784d832883
1brah1m0f <sixiibrahimov217@gmail.com>
2026-02-11 00:03:13 +0400

    Add sum_list function to calculate the sum of a list of floats

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Docstring for python_variable_annotations.5-sum_list"""
+
+
+def sum_list(input_list: float) -> float:
+    s = 0
+    """Docstring for sum_list"""
+    for i in input_list:
+        s = s + i
+    return float(s)
