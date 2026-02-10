26d8780f31d21a63aa1aac95e5f809504cb5c8d5
1brah1m0f <sixiibrahimov217@gmail.com>
2026-02-11 03:04:26 +0400

    Add to_kv function for converting key-value pairs with type annotations

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Docstring for python_variable_annotations.7-to_kv"""
+
+from typing import List, Union
+
+def to_kv(k: str, union(int, float)) -> tuple[str, float]:
+    return (k, float(v * v))
