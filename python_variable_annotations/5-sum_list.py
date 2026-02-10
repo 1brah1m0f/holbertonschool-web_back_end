2c5fb143a9ad81e5e54e1cd9c434a6843dd209b6
1brah1m0f <sixiibrahimov217@gmail.com>
2026-02-11 00:06:46 +0400

    Fix type annotation for input_list parameter in sum_list function

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -2,7 +2,7 @@
 """Docstring for python_variable_annotations.5-sum_list"""
 
 
-def sum_list(input_list: float) -> float:
+def sum_list(input_list: list[float]) -> float:
     s = 0
     """Docstring for sum_list"""
     for i in input_list:
