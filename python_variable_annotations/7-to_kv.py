322c8032fdab63b878cf008279dc672d791a2939
1brah1m0f <sixiibrahimov217@gmail.com>
2026-02-11 03:13:18 +0400

    Add a blank line for improved readability in to_kv function

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -3,6 +3,7 @@
 
 from typing import Tuple, Union
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Return a tuple with a string and the square of a number."""
     return (k, float(v * v))
