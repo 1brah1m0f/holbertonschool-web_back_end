6915e97ea118880d0d9be74508ea5c28b1919baf
1brah1m0f <sixiibrahimov217@gmail.com>
2026-02-11 00:07:44 +0400

    Refactor sum_list function: update docstring and simplify return statement

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-"""Docstring for python_variable_annotations.5-sum_list"""
-
+"""Module for python_variable_annotations 5-sum_list."""
 
 def sum_list(input_list: list[float]) -> float:
+    """Return the sum of a list of floats."""
     s = 0
-    """Docstring for sum_list"""
     for i in input_list:
-        s = s + i
-    return float(s)
+        s += i
+    return s
