f64dfab9da1b21a0fbeac085958cf51d0ec891cd
1brah1m0f <sixiibrahimov217@gmail.com>
2026-02-11 00:22:25 +0400

    Fix formatting by adding a blank line after module docstring in sum_list.py

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -2,6 +2,7 @@
 """Module for python_variable_annotations 5-sum_list."""
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
     """Return the sum of a list of floats."""
     return sum(input_list)
