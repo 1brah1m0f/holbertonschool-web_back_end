a046693a98793633dc8cd0d59239650b42c42344
1brah1m0f <sixiibrahimov217@gmail.com>
2026-02-16 16:58:32 +0400

    Add initial implementation of wait_random function and README file

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+
+    delay = random.uniform(0, max_delay)
+
+    await asyncio.time(delay)
+
+    return delay
