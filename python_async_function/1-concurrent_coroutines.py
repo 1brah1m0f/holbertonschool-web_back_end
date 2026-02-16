7d543fa95dd6e357f23cdfcc87012bdf3b72109e
1brah1m0f <sixiibrahimov217@gmail.com>
2026-02-16 18:19:54 +0400

    Enhance wait_n function signature and update docstring for parameters

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -2,13 +2,16 @@
 """Docstring for python_async_function.1-concurrent_coroutines"""
 
 import asyncio
+from typing import List
+
 wait_random = __import__('0-basic_async_syntax').wait_random
-async def wait_n(n: int, max_delay: int):
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Docstring for wait_n
-    :param n: Description
+    :param n: Təkrarlanma sayı
     :type n: int
-    :param max_delay: Description
+    :param max_delay: Maksimum gözləmə vaxtı
     :type max_delay: int
     """
     tasks = [wait_random(max_delay) for _ in range(n)]
@@ -17,4 +20,5 @@ async def wait_n(n: int, max_delay: int):
     for task in asyncio.as_completed(tasks):
         delay = await task
         delays.append(delay)
+        
     return delays
