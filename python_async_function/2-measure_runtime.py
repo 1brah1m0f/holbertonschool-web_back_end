e00dd4db4bf06442f46d6981b90b96f0f81765cc
1brah1m0f <sixiibrahimov217@gmail.com>
2026-02-18 14:52:13 +0400

    Add docstring to measure_time function for improved documentation

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Docstring for python_async_function.2-measure_runtime"""
 
 import time
 import asyncio
@@ -7,6 +8,16 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
+    """
+    Docstring for measure_time
+    
+    :param n: Description
+    :type n: int
+    :param max_delay: Description
+    :type max_delay: int
+    :return: Description
+    :rtype: float
+    """
     start = time.time()
 
     asyncio.run(wait_n(n, max_delay))
