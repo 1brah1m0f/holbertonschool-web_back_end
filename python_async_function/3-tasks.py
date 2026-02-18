b151c4373a1c582d5e1ed668d84f0cb849c3d968
1brah1m0f <sixiibrahimov217@gmail.com>
2026-02-18 15:32:39 +0400

    Fix function name and improve implementation in 3-tasks.py

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
 """Docstring for python_async_function.3-tasks"""
-
 import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
 
-task_wait_random = __import__('3-tasks').task_wait_random
-
-def task_wait_rando(max_delay: int) -> asyncio.Task:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """
-    Docstring for task_wait_rando
+    Docstring for task_wait_random
     :param max_delay: Description
     :type max_delay: int
     :return: Description
     :rtype: Task
     """
-    return max_delay
+    return asyncio.create_task(wait_random(max_delay))
