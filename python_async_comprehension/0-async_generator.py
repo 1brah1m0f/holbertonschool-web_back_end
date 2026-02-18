d43e649a9e0377bd9905596e35d64798c5ffa244
1brah1m0f <sixiibrahimov217@gmail.com>
2026-02-18 16:32:26 +0400

    Add async_generator implementation in 0-async_generator.py

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Docstring for python_async_comprehension.0-async_generator"""
+import asyncio
+import random
+
+async def async_generator():
+    """Docstring for async_generator"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
+
