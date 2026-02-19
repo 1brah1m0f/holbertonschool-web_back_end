f84ab6adf45d679dc3c01a134d32b1551d15b386
1brah1m0f <sixiibrahimov217@gmail.com>
2026-02-19 18:53:39 +0400

    Add async_comprehension implementation in 1-async_comprehension.py

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+async_generator = __import__('0-async_generator').async_generator
+
+async def async_comprehension():
+    return [number async for number in async_generator()]
