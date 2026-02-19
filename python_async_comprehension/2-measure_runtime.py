3ff61157d15fa2acc1c523269789183360cfe22f
1brah1m0f <sixiibrahimov217@gmail.com>
2026-02-19 19:16:21 +0400

    Refactor measure_runtime to use a loop for task creation

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -9,15 +9,12 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 async def measure_runtime() -> float:
     """Execute async_comprehension 4 times in parallel and measure runtime."""
-    
+
     start = time.perf_counter()
 
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
+    tasks = [async_comprehension() for _ in range(4)]
+
+    await asyncio.gather(*tasks)
 
     end = time.perf_counter()
 
