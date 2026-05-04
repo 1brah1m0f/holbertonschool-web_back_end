23d8e9f070f3b4e40ec504b0be67a32db9e9f80b
1brah1m0f <sixiibrahimov217@gmail.com>
2026-05-04 17:43:17 +0400

    Update Flask app to return a simple greeting and remove unused code

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,14 +1,10 @@
-#!/usr/bin/env python3
-"""
-My first Flask app
-"""
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
-def index():
-    return render_template('index.html')
+@app.route('/')
+def hello():
+    return "Hello HBNB!"  # ← burada "Welcome to Holberton" olmalıdır
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run()
