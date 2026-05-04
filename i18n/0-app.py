f9babf46accd419c51d90407a902333f4dea3208
1brah1m0f <sixiibrahimov217@gmail.com>
2026-05-04 17:44:44 +0400

    Update greeting message in Flask app to "Welcome to Holberton!"

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,10 +1,11 @@
+#!/usr/bin/python3
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello HBNB!"  # ← burada "Welcome to Holberton" olmalıdır
+    return "Welcome to Holberton!"
 
 if __name__ == "__main__":
     app.run()
