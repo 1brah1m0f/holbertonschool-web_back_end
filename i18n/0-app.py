a16c39f65def231338192eefbcaade6647e54a15
1brah1m0f <sixiibrahimov217@gmail.com>
2026-05-04 18:05:40 +0400

    Update Flask app to render index template instead of returning static message

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello world"
+def index():
+    return render_template('0-index.html')
 
 if __name__ == "__main__":
     app.run()
